c519ad08fd00d0dd125613af96376abda1994397
androborn <andrii.titov@gmail.com>
2021-10-17 01:07:09 +0300

    Task #4 resolved

---
+++ js/task-04.js
@@ -0,0 +1,22 @@
+// Счетчик состоит из спана и кнопок, которые, при клике, должны увеличивать и уменьшать его значение на единицу.
+
+// Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
+// Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
+// Обновляй интерфейс новым значением переменной counterValue.
+
+let counterValue = 0;
+
+const counterUi = document.querySelector('#value');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+
+function addition() {
+  counterUi.textContent = counterValue += 1;
+}
+
+function substraction() {
+  counterUi.textContent = counterValue -= 1;
+}
+
+incrementBtn.addEventListener('click', addition);
+decrementBtn.addEventListener('click', substraction);
