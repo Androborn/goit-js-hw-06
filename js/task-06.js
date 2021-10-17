13f912b2d0ab8a13141f8eeffc7da036bf8f10b7
androborn <andrii.titov@gmail.com>
2021-10-17 22:44:51 +0300

    Fix task #6 and change files structure
    *Fix class addition / replacement in task 6 depending on previous class attribute state
    *Move HTML files to a separate folder
    *Change related file paths accordingly

--- js/task-06.js
+++ js/task-06.js
@@ -8,10 +8,32 @@ const textInput = document.querySelector('#validation-input');
 
 textInput.addEventListener('blur', inputValidate);
 
+// function inputValidate(event) {
+//   if (textInput.classList.value === '') {
+//     if (event.currentTarget.value.length === +textInput.dataset.length) {
+//       return textInput.classList.add('valid');
+//     }
+
+//     textInput.classList.add('invalid');
+//   }
+
+//   if (textInput.classList !== '') {
+//     if (event.currentTarget.value.length === +textInput.dataset.length) {
+//       return textInput.classList.replace('invalid', 'valid');
+//     }
+
+//     textInput.classList.replace('valid', 'invalid');
+//   }
+// }
+
 function inputValidate(event) {
   if (event.currentTarget.value.length === +textInput.dataset.length) {
-    return textInput.classList.replace('invalid', 'valid');
+    return textInput.classList.value === ''
+      ? textInput.classList.add('valid')
+      : textInput.classList.replace('invalid', 'valid');
   }
 
-  textInput.classList.add('invalid');
+  textInput.classList.value === ''
+    ? textInput.classList.add('invalid')
+    : textInput.classList.replace('valid', 'invalid');
 }
