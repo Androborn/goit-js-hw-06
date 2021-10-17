8ebbe2b453630a301917ab6167f0cee247107b73
androborn <andrii.titov@gmail.com>
2021-10-17 13:16:33 +0300

    Task #9 resolved

--- js/task-08.js
+++ js/task-08.js
@@ -6,18 +6,6 @@
 // Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
 // Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
 
-// 1. define operated element +
-// 2. arrange operating function +
-// 2.1. prevent defaulst actions +
-// 2.2. destructurize form object +
-// 2.3. form object for collection +
-// 2.4. form condition +
-// 2.4.1. alert on emty fields on submit +
-// 2.5. collect names & values to object properties and values +
-// 2.6. log object on submit +
-// 2.7. reset form on submit +
-// 3. define event listener +
-
 const form = document.querySelector('.login-form');
 
 form.addEventListener('submit', handleSubmit);
@@ -34,8 +22,8 @@ function handleSubmit(event) {
   if (email.value === '' || password.value === '') {
     return alert('All the fields must be filled');
   }
-  valuesCollection['email'] = email.value;
-  valuesCollection['password'] = password.value;
+  valuesCollection[email.name] = email.value;
+  valuesCollection[password.name] = password.value;
 
   console.log(valuesCollection);
 
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
+
+// Для генерации случайного цвета используй функцию getRandomHexColor.
+
+const backgroundContainer = document.querySelector('body');
+const colorNamer = document.querySelector('.color');
+const colorChangeBtn = document.querySelector('button.change-color');
+
+colorChangeBtn.addEventListener('click', handleClickBgColorChange);
+
+function handleClickBgColorChange() {
+  const colorPicker = getRandomHexColor();
+  backgroundContainer.style.backgroundColor = colorPicker;
+  colorNamer.textContent = colorPicker;
+}
